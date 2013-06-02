91f706835c4f30182b752d9806aa448508ecbb3a
01moumooug <01moumooug@gmail.com>
2013-06-02 11:51:59 +0900

    편집 url을 따로 설정할 수 있게 바꿈. w2notes 파일에서 편집 기능을 제거함. 파일 편집 방법은 NotesConfig.sample.pm을 참고

---
+++ edit.sample.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use FindBin;
+use lib	"$FindBin::RealBin/.";
+
+use Subroutines;
+use NotesConfig;
+use File::Spec::Functions qw/ catfile /;
+
+{
+	my $file = $ARGV[0];
+	exit unless $file =~ m|^edit://|;
+
+	$file =~ s|edit://||;
+	$file = url_decode($file);
+
+	system('/usr/bin/leafpad', catfile($_CONF{'root'},$file));
+	exec('w2notes update');
+}
